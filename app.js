0a7749796d8c0d3b91d41484fe4b4384673e1fba
Angelito <litsdionisio@gmail.com>
2024-11-26 11:41:48 +0800

    Removed contact and downloadCV buttons

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger-btn');
 const icon = document.querySelector('.hamburger-icon');
 const list = document.querySelector('.menu-list');
-const contactBtn = document.querySelector('.btn-1');
+// const contactBtn = document.querySelector('.btn-1');
 // const downloadCV = document.querySelector('.btn-2');
 
 hamburger.addEventListener('click', () => {
@@ -9,7 +9,7 @@ hamburger.addEventListener('click', () => {
     list.classList.toggle('open');
 })
 
-contactBtn.addEventListener('click', () => location.href='./#contact');
+// contactBtn.addEventListener('click', () => location.href='./#contact');
 
 // downloadCV.addEventListener('click', () => {
 //     const link = document.createElement('a');
